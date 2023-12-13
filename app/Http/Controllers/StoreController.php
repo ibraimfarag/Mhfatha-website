88f296d517cfcb63a278a05203520d734be1df34
ibraimfarag <ib.farag@gmail.com>
2023-12-13 20:41:34 +0200

    -D

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -394,7 +394,7 @@ class StoreController extends Controller
                 'name' => $store->name,
                 'photo' => $store->photo,
                 'distance' => $store->distance,
-                'discounts' => $store->discounts->where('Discounts_status', 'working')->where('is_deleted', 0),
+                'discounts' => $store->discounts,
             ];
         });
         
