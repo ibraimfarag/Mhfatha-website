6d68171b9d30bbb6a0f913ed9e04bcc2110b1250
ibraimfarag <ib.farag@gmail.com>
2024-01-25 13:03:01 +0200

    -e

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,7 @@ Route::middleware('auth:api')->group(function () {
 
 
     Route::get('/user', [UserController::class, 'getUserInfoApi']);
-    Route::post('/regions', [UserController::class, 'getRegionsAndCitiesApi']);
+    Route::get('/regions', [UserController::class, 'getRegionsAndCitiesApi']);
     Route::post('/auth/update', [UserController::class, 'updateProfileWithOtp']);
     Route::post('/nearby', [StoreController::class, 'nearbyApi']);
     Route::post('/store', [StoreController::class, 'storeInfoApi']);
