115f35142c987a02da61e4424fc9ccd75246a12b
ibraimfarag <ib.farag@gmail.com>
2024-02-05 21:34:06 +0200

    -e

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -637,7 +637,7 @@ class StoreController extends Controller
 
 
         // Query to get a list of unique regions and categories
-        $regionListQuery = Store::distinct('region')->pluck('region')->toArray();
+        $regionListQuery = Store::where('verifcation', 1)->distinct('region')->pluck('region')->toArray();
 
         $regions = Region::whereIn('id', $regionListQuery)->pluck('region_' . $lang, 'id')->toArray();
 
