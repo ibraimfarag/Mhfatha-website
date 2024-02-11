ee0205d6f90796ae6197abb5543b0212ee266ffc
ibraimfarag <ib.farag@gmail.com>
2024-02-11 22:51:34 +0200

    -RemoveNotification

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
+
                 'host' => 'mhfatha',
                 'port' => 6001,
                 'scheme' => 'http'
