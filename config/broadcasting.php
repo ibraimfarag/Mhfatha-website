701135c7449b81dcccafbdf0c3b8f17d8a1f4dbb
ibraimfarag <ib.farag@gmail.com>
2024-02-11 21:58:20 +0200

    Notification

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => 'mhfatha',
+                'host' => 'mhfatha.net',
                 'port' => 6001,
                 'scheme' => 'http'
             ],
