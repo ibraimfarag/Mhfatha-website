5503f2a755dc0569307ab570d38be87ec773f08e
ibraimfarag <ib.farag@gmail.com>
2024-03-05 11:10:22 +0200

    update-device-info

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -10,6 +10,11 @@ class Request extends Model
 
     use HasFactory;
 
-    protected $fillable = ['user_id','store_id', 'type', 'data', 'approved'];
+    protected $fillable = ['user_id', 'store_id', 'type', 'data', 'approved'];
 
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id'); // Assuming user_id is the foreign key in the requests table
+    }
 }
