ebdc4a9d0dcefa5b2ea4edec14bbcdd53852acce
ibraimfarag <ib.farag@gmail.com>
2024-03-06 20:48:24 +0200

    update-device-info

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -170,7 +170,7 @@ class RequestsController extends Controller
 
     public function sendPushNotification(Request $request){
 
-        $credentialsFilePath = public_path("firebase/fcm.json");
+        $credentialsFilePath = "firebase/fcm.json";
         $client = new \Google_Client();
         $client->setAuthConfig($credentialsFilePath);
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
