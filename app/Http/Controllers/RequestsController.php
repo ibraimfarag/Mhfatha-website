ef9fd49ef75efd0727c0bedb38eb8553c4b8ec71
ibraimfarag <ib.farag@gmail.com>
2024-03-06 21:57:10 +0200

    update-device-info

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -169,23 +169,14 @@ class RequestsController extends Controller
     }
     public function sendPushNotification(Request $request)
     {
-        // Load the JSON service account credentials
-        $jsonKey = [
-            'web' => [
-                'client_id' => '197190218958-2416ottf4hmh9abkgmafe52sni2g2p1m.apps.googleusercontent.com',
-                'project_id' => 'mhfaata',
-                'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
-                'token_uri' => 'https://oauth2.googleapis.com/token',
-                'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
-                'client_secret' => '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'
-            ]
-        ];
+
+
 
         // Initialize a new Google_Client
         $client = new \Google\Client();
 
         // Set the authentication configuration using the provided JSON data
-        $client->setAuthConfig($jsonKey);
+        $client->setAuthConfig(base_path('public/firebase/mhfaata.json'));
 
         // Add the necessary scope for Firebase Messaging
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
