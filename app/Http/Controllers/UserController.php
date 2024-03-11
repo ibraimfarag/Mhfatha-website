26bc72af976781206afb826d7d7fc9814a6db9a2
ibraimfarag <ib.farag@gmail.com>
2024-03-11 23:31:27 +0200

    update-device-info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -812,8 +812,8 @@ class UserController extends Controller
         $mobilenumberAR =  $user->mobile . '(966+)';
 
         // Step 2: Verify the OTP
-        $storedOtp = "12345";
-        // $storedOtp = rand(10000, 99999); 
+        // $storedOtp = "12345";
+        $storedOtp = rand(10000, 99999); 
         $userLanguage = $user->lang;
         
         // Set $lang based on the user's language
