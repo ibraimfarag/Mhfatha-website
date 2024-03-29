1e998017d6e76ab5d51b2ac2ef723edf22154a34
ibraimfarag <ib.farag@gmail.com>
2024-03-29 23:22:55 +0200

    update-device-info

--- app/Http/Controllers/TermsAndConditionsPolicyController.php
+++ app/Http/Controllers/TermsAndConditionsPolicyController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\TermsAndConditionsPolicy;
+use App\Models\TermsAndConditionsPolicy;
 
 class TermsAndConditionsPolicyController extends Controller
 {
