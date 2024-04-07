9e4bb2c26d7c2fd47d2222fcc2461f458938490d
ibraimfarag <ib.farag@gmail.com>
2024-04-08 01:44:59 +0200

    update-device-info

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -1297,12 +1297,12 @@ class StoreController extends Controller
 
         // If any relevant field has changed, create a request for approval
         if ($nameChanged || $photoChanged || $taxNumberChanged || $categoryIdChanged || $regionChanged || $mobileNumberChanged) {
-            if ($pendingRequests) {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => ($lang === 'ar') ? 'نآسف، لا يزال طلبك السابق قيد الانتظار.' : 'Sorry, your last  request is still pending approval.',
-                ], 422);
-            }
+            // if ($pendingRequests) {
+            //     return response()->json([
+            //         'status' => 'error',
+            //         'message' => ($lang === 'ar') ? 'نآسف، لا يزال طلبك السابق قيد الانتظار.' : 'Sorry, your last  request is still pending approval.',
+            //     ], 422);
+            // }
             // Delete the old image if it exists
             if ($store->photo) {
                 $oldImagePath = public_path('store_images/' . $store->photo);
