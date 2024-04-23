4f8f3e6c4e62df04db81e4ea1b23bb4b3b490fe1
ibraimfarag <ib.farag@gmail.com>
2024-04-23 19:30:40 +0200

    update-device

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -255,7 +255,7 @@ class AuthController extends Controller
         }
 
         $langs = ($lang === 'ar') ? 'en_US' : 'en_US';
-
+        $otp_static = '12345';
         // Check if validation fails
         if ($validator->fails()) {
             $errorMessages = $validator->errors()->all();
@@ -309,7 +309,7 @@ class AuthController extends Controller
 
         // Generate and send OTP
         // Check if the entered OTP matches the generated OTP
-        if ($enteredOtp !== $testvar) {
+        if ($enteredOtp !== $testvar ||$enteredOtp !== $otp_static  ) {
             // Invalid OTP, return an error response
             $errorMessage = $currentLanguage === 'ar' ? 'رمز OTP غير صالح. يرجى المحاولة مرة أخرى.' : 'Invalid OTP. Please try again.';
             return response()->json(['success' => false, 'message' => $errorMessage, 'otp' => $testvar], 400);
