dcb42ecf3a6739d6e7010f63ebf1630bc78f23dd
ibraimfarag <ib.farag@gmail.com>
2024-06-04 01:09:14 +0300

    time

--- app/Http/Middleware/ThroughputLogger.php
+++ app/Http/Middleware/ThroughputLogger.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\GeoIP;
 use Illuminate\Support\Str;
 
 class ThroughputLogger
@@ -18,10 +19,13 @@ class ThroughputLogger
         $endTime = microtime(true);
         $processingTime = $endTime - $startTime;
 
-        // Log::channel('api_throughput')->info("Request to {$request->method()} {$request->fullUrl()} processed in {$processingTime} seconds.");
+        $requestBytes = strlen($request->getContent()); // Get request body size in bytes
+        $clientIp = $request->ip(); // Get client IP address
+
         if (Str::startsWith($request->path(), 'api')) {
-            Log::channel('api_throughput')->info("API Request to {$request->method()} {$request->fullUrl()} processed in {$processingTime} seconds.");
+            Log::info("API Request to {$request->method()} {$request->fullUrl()} processed in {$processingTime} seconds. Requested Bytes: {$requestBytes}, Client IP: {$clientIp}");
         }
+
         return $response;
     }
 }
