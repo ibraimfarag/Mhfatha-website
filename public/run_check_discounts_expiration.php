2d1402aeb5bd1811c344bdbadfd567203cd94345
ibraimfarag <ib.farag@gmail.com>
2024-06-04 12:44:20 +0300

    time

--- public/run_check_discounts_expiration.php
+++ public/run_check_discounts_expiration.php
@@ -1,7 +1,7 @@
 <?php
 
 // Define the URL of your Laravel application dynamically
-$laravelUrl = 'http://mhfatha.net/api/update-discounts';
+// $laravelUrl = 'http://mhfatha.net/api/update-discounts';
 
 // Make a GET request to the route using file_get_contents
 $response = file_get_contents($laravelUrl);
