bc4c251aa55219a782128937b2d59839041f49dc
ibrahim ahmed <84781424+ibraimfarag@users.noreply.github.com>
2024-10-10 18:33:35 +0300

    land

---
+++ resources/views/FrontEnd/soon.blade.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<!--
+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+AUTHOR : NCode.Art
+PROJECT : NC-Hold Coming-Soon Page
+VERSION : 2.0
+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-->
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Mhfatha is a new app for discovering nearby discounts and exclusive offers in Saudi Arabia. Coming soon with a clean, user-friendly design, Mhfatha will help merchants and users connect for the best local deals. Stay tuned!">
+    <meta name="keywords" content="Mhfatha,coming soon,discounts,offers,nearby discounts,Saudi Arabia,QR code offers,local deals,merchant discounts,shopping app,coming soon page">
+    <meta name="author" content="Mhfatha Team">
+    <title>Mhfatha - Coming Soon</title>
+        
+	<!-- [ FONT-AWESOME ICON ] -->
+<!-- Font Awesome -->
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/lib/font-awesome-4.3.0/css/font-awesome.min.css') }}">
+
+<!-- Plugin Stylesheets -->
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/css/animate.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/css/owl.carousel.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/css/magnific-popup.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/css/jquery.mCustomScrollbar.min.css') }}">
+
+<!-- Default Stylesheets -->
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('FrontEnd/assets/soon/images/lo_go.png') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/lib/bootstrap/css/bootstrap-theme.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/lib/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/css/reset.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/css/structure.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/css/main-style.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/css/star-animation.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/assets/soon/css/responsive.css') }}">
+
+
+</head>
+<body class="single-image">
+	
+	<div class="page-loader-wrapper">
+		<div class="loader">
+			
+		</div>
+	</div>
+
+	<!-- [ MAIN-WRAPPER ] -->
+	<div id="main-wrapper">
+
+		<!-- [ MENU-ICON ] -->
+		<!-- [ MENU-ICON END ] -->
+
+		<!-- [ PAGE-SECTION ] -->
+        <main id="main" class="background" data-image="{{ asset('FrontEnd/assets/soon/images/stars-bg.png') }}">
+
+			<!-- STAR ANIMATION -->
+			<div class="bg-animation">
+				<div id='stars'></div>
+				<div id='stars2'></div>
+				<div id='stars3'></div>
+				<div id='stars4'></div>
+			</div>
+
+			<!-- [ BACKGROUND-OVERLAY ] -->
+			<div class="overlay" style="background-color: rgba(0,0,0,0.3)"></div>
+			<!-- [ BACKGROUND-OVERLAY END ] -->
+
+			<div class="nc-content-section index nc-active vhm">
+
+				<!-- [ HOME-PAGE ] -->
+				<div id="home-page" class="page-wrapper vhm-item active-home anim s01">
+					<div class="inner-wrp vhm">
+						
+						<!-- [ LOGO ] -->
+						<div class="logo ac anim fadeInUp s01 delay-0-5s">
+							<div class="logo-wrp">
+								<img src="{{ asset('FrontEnd/assets/soon/images/lo_go.png') }}" alt="Logo">
+							</div>
+						</div>
+						<!-- [ LOGO END ] -->
+						<!-- [ TAGLINE ] -->
+						<div class="container vhm-item">
+							<div class="tagline ac anim fadeInUp s01 delay-0-6s">
+                                <div class="">
+                                    <img src="{{ asset('FrontEnd/assets/soon/images/light.png') }}" alt="Logo" style="width: 30%;">
+                                </div>
+								<!-- ANIMATION TEXT -->
+								<div class="carousel-widget info-box-carousel" data-itemrange="false" data-in="flipInX" data-out="slideOutDown" data-autoplay="true" data-loop="true" data-items="1" data-mouseDrag="false" data-touchDrag="false">
+									<div class="carousel">
+										<div class="owl-carousel owl-theme">
+                                            <div class="item">
+                                                <h2>اكتشف الخصومات</h2>
+                                            </div>
+                                        
+                                            <div class="item">
+                                                <h2>عروض حصرية من المتاجر القريبة</h2>
+                                            </div>
+                                        
+                                            <div class="item">
+                                                <h2>!QR سهولة الاستخدام مع مسح </h2>
+                                            </div>
+                                        </div>
+                                        
+							
+									</div>
+								</div>
+								<span class="hr-line"></span>
+								<p class="anim fadeInUp s01 delay-0-7s">قريباً! استعد لاكتشاف الخصومات والعروض الحصرية من المتاجر القريبة في المملكة العربية السعودية مع تطبيق محفظة.<br>
+                                    تابعنا للحصول على المزيد من التحديثات قريباً</p>
+							</div>
+                            <span class="hr-line"></span>
+                            <p dir="rtl"  class="anim fadeInUp s01 delay-0-7s"style="
+                            text-align: center;
+                            font-size: 20px;
+                            margin: 20px;
+                        ">
+                                للتواصل مع التسويق: <a style="color: #ffffff;" href="mailto:marketing@mhfatha.net">marketing@mhfatha.net</a> &nbsp;|&nbsp; 
+                                للتواصل مع الدعم: <a style="color: #ffffff;" href="mailto:support@mhfatha.net">support@mhfatha.net</a> &nbsp;|&nbsp; 
+                                للتواصل عبر الواتساب: <a dir="ltr"  style="color: #ffffff;" href="https://wa.me/966563474766" target="_blank">+966 56 347 4766</a>
+                            </p>
+                            
+                            
+                            
+							<!-- [ COPYRIGHT ] -->
+							{{-- <div class="copyrights ac anim fadeInUp s01 delay-1s">
+								<span><i class="fa fa-copyright"></i></span>2015 Ncode.Art
+							</div> --}}
+							<!-- [ COPYRIGHT END ] -->
+							
+						</div>
+						<!-- [ TAGLINE END ] -->
+
+						<!-- [ SOCIAL ICON ] -->
+						{{-- <div class="social-icon ac anim fadeInUp s01 delay-0-9s">
+							<a class="icon vhm" href="#">
+								<span class="box"></span>
+								<i class="fa fa-facebook vhm-item"></i>
+							</a>
+							<a class="icon vhm" href="#">
+								<span class="box"></span>
+								<i class="fa fa-twitter vhm-item"></i>
+							</a>
+							<a class="icon vhm" href="#">
+								<span class="box"></span>
+								<i class="fa fa-linkedin vhm-item"></i>
+							</a>
+						</div> --}}
+						<!-- [ SOCIAL ICON END ] -->
+
+						<!-- [ SUBSCRIBE ] -->
+                        <a href="mailto:support@mhfatha.net">
+                            <button type="button"  class="icon-subcribe trigger vhm ac anim fadeInUp s01 delay-0-9s">
+                                <i class="fa fa-envelope-o vhm-item"></i>
+                            </button>
+                        </a>
+                                                <!-- [ SUBSCRIBE END ] -->
+                                                {{-- data-dialog="somedialog" --}}
+					</div>
+				</div>
+				<!-- [ HOME-PAGE END ] -->
+
+				<!-- [ AJAX-PAGE ] -->
+				<div id="ajax-page" class="page-wrapper vhm-item">
+					
+				</div>
+				<!-- [ AJAX-PAGE END ] -->
+
+			</div>
+
+			<!-- [ OC-CLICK OVERLAY ] -->
+			<div class="nc-overlay"></div>
+			<!-- [ OC-CLICK OVERLAY END ] -->
+
+		</main>
+		<!-- [ PAGE-SECTION END ] -->
+
+		<!-- [ NAVIGATION ] -->
+		<nav class="nc-menu-container" id="nc-menu">
+			<header class="nav-header vm">
+				<a href="#0" class="nc-menu-close"></a>
+			</header>
+
+			<ul id="navigation" class="nc-menu">
+				<li class="nc-active" data-menu="index">
+					<a class="link-box vhm nav-link" data-page="home.html" href="#home">
+						<span class="vhm-item inner-wrp">
+							<span class="icon"><i class="fa fa-home"></i></span>
+							<span class="text">Home</span>
+						</span>
+					</a>
+				</li>
+
+				<li data-menu="about">
+					<a class="odd link-box vhm nav-link" data-page="pages/about.html" href="#about">
+						<span class="vhm-item inner-wrp">
+							<span class="icon"><i class="fa fa-info"></i></span>
+							<span class="text">About</span>
+						</span>
+					</a>
+				</li>
+
+				<li data-menu="our-team">
+					<a class="odd link-box vhm nav-link" data-page="pages/our-team.html" href="#our-team">
+						<span class="vhm-item inner-wrp">
+							<span class="icon"><i class="fa fa-users"></i></span>
+							<span class="text">Team</span>
+						</span>
+					</a>
+				</li>
+
+				<li data-menu="services">
+					<a class="odd link-box vhm nav-link" data-page="pages/services.html" href="#services">
+						<span class="vhm-item inner-wrp">
+							<span class="icon"><i class="fa fa-cogs"></i></span>
+							<span class="text">Services</span>
+						</span>
+					</a>
+				</li>
+
+				<li data-menu="portfolio-2">
+					<a class="odd link-box vhm nav-link" data-page="pages/video-portfolio.html" href="#portfolio-2">
+						<span class="vhm-item inner-wrp">
+							<span class="icon"><i class="fa fa-camera"></i></span>
+							<span class="text">Portfolio</span>
+						</span>
+					</a>
+				</li>
+
+				<li data-menu="package">
+					<a class="link-box vhm nav-link" data-page="pages/package.html" href="#package">
+						<span class="vhm-item inner-wrp">
+							<span class="icon vhm"><i class="fa fa-usd vhm-item"></i></span>
+							<span class="text">Package</span>
+						</span>
+					</a>
+				</li>
+
+				<li data-menu="time">
+					<a class="link-box vhm nav-link" data-page="pages/time.html" href="#time">
+						<span class="vhm-item inner-wrp">
+							<span class="icon"><i class="fa fa-clock-o"></i></span>
+							<span class="text">When</span>
+						</span>
+					</a>
+				</li>
+
+				<li data-menu="contact">
+					<a class="odd link-box vhm nav-link" data-page="pages/contact.html" href="#contact">
+						<span class="vhm-item inner-wrp">
+							<span class="icon"><i class="fa fa-envelope"></i></span>
+							<span class="text">Contact</span>
+						</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- [ NAVIGATION END ] -->
+
+		<!-- [ SUBSCRIBE-FORM ] -->
+		<div id="somedialog" class="dialog">
+			<div class="dialog__overlay"></div>
+			<div class="dialog__content">
+				<!-- [ TITLE ] -->
+				<section class="title-wrapper anim fadeInUp s01 delay-0-2s">
+					<h3 class="h3 title"> Notify Me </h3>
+					<span class="line"></span>
+					<p>
+						Enter your email to get notified.
+					</p>
+				</section>
+
+				<div class="subscribe ac anim fadeInUp s01 delay-0-8s">
+					<form id="notifyMe" class="subscription-form clear form-field-wrapper" method="POST" action="notify-me.php">
+						<input type="text" class="form-control" name="email" placeholder="Subscribe">
+						<button type="submit" id="submit" class="icon vhm"><i class="fa fa-envelope-o vhm-item"></i></button>
+					</form>
+				</div>
+				<button class="action" data-dialog-close><i class="fa fa-times"></i></button>
+			</div>
+		</div>
+		<!-- [ SUBSCRIBE-FORM END ] -->
+
+	</div>
+	<!-- [ MAIN-WRAPPER END ] -->
+
+<!-- Default Script -->
+<script src="{{ asset('FrontEnd/assets/soon/js/jquery-1.11.3.min.js') }}"></script>
+
+<!-- Plugin Scripts -->
+<script src="{{ asset('FrontEnd/assets/soon/js/plugins.js') }}"></script>
+<script src="{{ asset('FrontEnd/assets/soon/js/classie.js') }}"></script>
+<script src="{{ asset('FrontEnd/assets/soon/js/dialogFx.js') }}"></script>
+
+<!-- Common Script -->
+<script src="{{ asset('FrontEnd/assets/soon/js/common.js') }}"></script>
+
+</body>
+</html>
